ad412a32929e609f7c789741ed7c89f5d28b6810
Daria Chernovol <fabulus.kg.dk@gmail.com>
2020-02-03 13:04:22 +0600

    fixed messages.js

--- app/messages.js
+++ app/messages.js
@@ -6,15 +6,11 @@ router.get('/', (req, res) => {
     res.send('List of messages will be here')
 });
 
-router.get('/messages', (req, res) => {
-    res.send('List of messages will be here')
-});
-
-router.get('/messages/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     res.send(`A single message by ID=${req.params.id} will be here`)
 });
 
-router.post('/messages', (req, res) => {
+router.post('/', (req, res) => {
     console.log(req.body);
     res.send('Will create new message here')
 });
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const products = require('./app/messages');
+const messages = require('./app/messages');
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
 
-app.use('/messages', products);
+app.use('/messages', messages);
 
 app.listen(port, () => {
     console.log(`Server started on ${port} port!`)
