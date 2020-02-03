7a5dea9ec2f6b655314e8cd9456d5fbdc545ce05
Daria Chernovol <fabulus.kg.dk@gmail.com>
2020-02-03 14:02:59 +0600

    added the ability to read directory with files

--- app/messages.js
+++ app/messages.js
@@ -2,20 +2,25 @@ const express = require('express');
 const fs = require('fs');
 
 const router = express.Router();
+const path = './messages';
+
 
 router.get('/', (req, res) => {
-    res.send('List of messages will be here')
-});
-
-router.get('/:id', (req, res) => {
-    res.send(`A single message by ID=${req.params.id} will be here`)
+    fs.readdir('./messages', (err, files) => {
+        console.log(files);
+        files.forEach(file => {
+            console.log(path + '/' + file);
+        });
+        res.send(files);
+    })
 });
 
 router.post('/', (req, res) => {
     const date = new Date();
-    const message = {...req.body, "datetime": date};
+    const datetime = date.toISOString();
+    const message = {...req.body, "datetime": datetime};
 
-    const fileName = `./messages/${date}.txt`;
+    const fileName = `./messages/${datetime}.txt`;
 
     const data = JSON.stringify(message, null, 2);
 
